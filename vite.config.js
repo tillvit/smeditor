fa67b7f88846ffb2040acdfc9ebc746a85345371
tillvit <lucas.j.tan@gmail.com>
2023-12-19 20:25:56 -0800

    Change app type to mpa

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/smeditor",
+  appType: "mpa",
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
