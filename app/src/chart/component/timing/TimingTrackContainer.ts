d2519b4fb35adecc1f9c0e6c1da0bfc65227dff5
tillvit <lucas.j.tan@gmail.com>
2024-06-06 16:59:03 +0800

    Fix timing tracks not disappearing when visibility toggled

--- app/src/chart/component/timing/TimingTrackContainer.ts
+++ app/src/chart/component/timing/TimingTrackContainer.ts
@@ -323,12 +323,15 @@ export class TimingTrackContainer
       this.renderer.chartManager.editTimingMode != EditTimingMode.Off &&
       this.renderer.chartManager.getMode() == EditMode.Edit
 
+    this.tracks.children.forEach(child => (child.visible = false))
+
     let x = -this.renderer.chart.gameType.notefieldWidth * 0.5 - 128
     for (let i = leftTypes.length - 1; i >= 0; i--) {
       const type = leftTypes[i]
       const track =
         this.tracks.getChildByName<TimingTrack>(type) ??
         this.createTrack(type, x)
+      track.visible = true
       if (track.lastX != x) {
         track.lastX = x
         track.targetAlpha = i % 2 == 0 ? 0.1 : 0
@@ -455,7 +458,12 @@ export class TimingTrackContainer
     }
 
     for (const [event, box] of this.timingBoxMap.entries()) {
-      if (event.beat < fromBeat || event.beat > toBeat) {
+      if (
+        event.beat < fromBeat ||
+        event.beat > toBeat ||
+        (!Options.chart.timingEventOrder.left.includes(event.type) &&
+          !Options.chart.timingEventOrder.right.includes(event.type))
+      ) {
         this.timingBoxMap.delete(event)
         if (box.popup?.persistent) box.popup?.detach()
         else box.popup?.close()
