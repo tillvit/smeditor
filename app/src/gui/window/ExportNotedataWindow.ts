79bc71b9cf1f6f2cc8468d971b46cd541c72f488
tillvit <lucas.j.tan@gmail.com>
2024-08-14 14:29:02 -0700

    Add NotITG note types to ExportNotedataWindow

--- app/src/gui/window/ExportNotedataWindow.ts
+++ app/src/gui/window/ExportNotedataWindow.ts
@@ -8,11 +8,34 @@ interface ExportNotedataOptions {
   options: Record<string, boolean>
 }
 
-const OPTION_NAMES: Record<string, string> = {
-  columnOneBased: "1-indexed column numbers",
-  lengthAsNumberIndex: "Store length in integer keys",
-  padNumbers: "Pad numbers",
-  minify: "Minify",
+const OPTION_NAMES: Record<string, { label: string; tooltip?: string }> = {
+  columnOneBased: {
+    label: "1-indexed column numbers",
+    tooltip: "Start counting column numbers from 0 instead of 1.",
+  },
+  lengthAsNumberIndex: {
+    label: "Store length in integer keys",
+    tooltip:
+      'Store the length of holds as the last item in each table, instead of using the "length" key.',
+  },
+  padNumbers: {
+    label: "Pad numbers",
+    tooltip: "Pad decimals with trailing zeros.",
+  },
+  minify: { label: "Minify", tooltip: "Remove all newlines and spaces." },
+  notitgNoteTypes: {
+    label: "Use NotITG Note Types",
+    tooltip: 'Use NotITG note types. (Tap = 1, Hold = 2, Mine = "M")',
+  },
+}
+
+const NOTITG_TYPES: Record<string, string> = {
+  Tap: "1",
+  Hold: "2",
+  Roll: "4",
+  Mine: '"M"',
+  Lift: '"L"',
+  Fake: '"F"',
 }
 
 export class ExportNotedataWindow extends Window {
@@ -33,6 +56,7 @@ export class ExportNotedataWindow extends Window {
       lengthAsNumberIndex: false,
       padNumbers: false,
       minify: false,
+      notitgNoteTypes: false,
     },
   }
 
@@ -101,13 +125,15 @@ export class ExportNotedataWindow extends Window {
     Object.keys(this.exportOptions.include).forEach(name => {
       const checkbox = document.createElement("input")
       checkbox.type = "checkbox"
+      checkbox.id = "en-i-" + name
       checkbox.checked = this.exportOptions.include[name]
       checkbox.onchange = () => {
         this.exportOptions.include[name] = checkbox.checked
         this.export()
       }
-      const optionLabel = document.createElement("div")
+      const optionLabel = document.createElement("label")
       optionLabel.classList.add("export-label")
+      optionLabel.htmlFor = checkbox.id
       optionLabel.innerText = name
 
       const container = document.createElement("div")
@@ -124,20 +150,28 @@ export class ExportNotedataWindow extends Window {
 
     Object.keys(this.exportOptions.options).forEach(name => {
       const checkbox = document.createElement("input")
+      checkbox.id = "en-o-" + name
       checkbox.type = "checkbox"
       checkbox.checked = this.exportOptions.options[name]
       checkbox.onchange = () => {
         this.exportOptions.options[name] = checkbox.checked
         this.export()
       }
-      const optionLabel = document.createElement("div")
+      const optionLabel = document.createElement("label")
       optionLabel.classList.add("export-label")
-      optionLabel.innerText = OPTION_NAMES[name]
+      optionLabel.htmlFor = checkbox.id
+      optionLabel.innerText = OPTION_NAMES[name].label
 
       const container = document.createElement("div")
       container.replaceChildren(checkbox, optionLabel)
       container.classList.add("export-option")
       options.appendChild(container)
+
+      if (OPTION_NAMES[name].tooltip !== undefined) {
+        tippy(container, {
+          content: OPTION_NAMES[name].tooltip,
+        })
+      }
     })
 
     container.replaceChildren(options, output)
@@ -161,7 +195,16 @@ export class ExportNotedataWindow extends Window {
               str += note.col + 1 + ","
             else str += note.col + ","
           }
-          if (this.exportOptions.include.Type) str += '"' + note.type + '",'
+          if (this.exportOptions.include.Type) {
+            if (
+              this.exportOptions.options.notitgNoteTypes &&
+              NOTITG_TYPES[note.type] !== undefined
+            ) {
+              str += NOTITG_TYPES[note.type] + ","
+            } else {
+              str += '"' + note.type + '",'
+            }
+          }
           if (this.exportOptions.include.Quantization) str += note.quant + ","
           if (this.exportOptions.include.Length && isHoldNote(note)) {
             if (this.exportOptions.options.lengthAsNumberIndex)
