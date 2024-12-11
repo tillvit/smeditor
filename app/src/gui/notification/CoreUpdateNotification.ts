51d6f36e9b8c2604564cf732d2d9c7ee55f56c44
tillvit <lucas.j.tan@gmail.com>
2024-12-10 22:31:31 -0500

    Remove offline use popoup, reload on refresh

--- app/src/gui/notification/CoreUpdateNotification.ts
+++ app/src/gui/notification/CoreUpdateNotification.ts
@@ -16,6 +16,7 @@ export class CoreUpdateNotification extends UpdateNotification {
           type: "confirm",
           callback: () => {
             callback()
+            window.location.reload()
           },
         },
       ],
