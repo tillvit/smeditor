f8f0a650399ee94963729169109e323ade440e26
tillvit <lucas.j.tan@gmail.com>
2025-05-27 14:46:37 -0700

    Use path module if available
    Fixes file picker relative path on windows

--- app/src/util/Path.ts
+++ app/src/util/Path.ts
@@ -1,4 +1,7 @@
 export function basename(path: string, ext?: string) {
+  if (window.nw) {
+    return nw.require("path").basename(path, ext) as string
+  }
   let f = posixSplitPath(path)[2]
   if (ext && f.slice(-1 * ext.length) === ext) {
     f = f.slice(0, f.length - ext.length)
@@ -7,6 +10,9 @@ export function basename(path: string, ext?: string) {
 }
 
 export function dirname(path: string) {
+  if (window.nw) {
+    return nw.require("path").dirname(path) as string
+  }
   const result = posixSplitPath(path)
   const root = result[0]
   let dir = result[1]
@@ -19,6 +25,9 @@ export function dirname(path: string) {
 }
 
 export function extname(path: string) {
+  if (window.nw) {
+    return nw.require("path").extname(path) as string
+  }
   return posixSplitPath(path)[3]
 }
 
