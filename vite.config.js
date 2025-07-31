2d40de7bed28655684d696a4c0838ac10532c89f
tillvit <lucas.j.tan@gmail.com>
2025-07-31 11:35:48 -0700

    Move preview files, don't cache mp4

--- vite.config.js
+++ vite.config.js
@@ -31,6 +31,7 @@ export default defineConfig({
       includeManifestIcons: false,
       workbox: {
         globPatterns: ['**/*.{js,css,html}', 'assets/**/*'],
+        globIgnores: ['**/*.mp4'],
         ignoreURLParametersMatching: [/^flags/, /^url/, /^chartIndex/, /^chartType/],
         maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
         navigateFallback: null,
