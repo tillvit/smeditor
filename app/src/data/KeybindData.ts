ec136d1c2e00474f65b800c8a4c549bff05ac8a8
tillvit <lucas.j.tan@gmail.com>
2025-09-04 10:24:41 -0400

    remove custom script keybind data

--- app/src/data/KeybindData.ts
+++ app/src/data/KeybindData.ts
@@ -12,7 +12,6 @@ import { AboutWindow } from "../gui/window/AboutWindow"
 import { CaptureWindow } from "../gui/window/CaptureWindow"
 import { ChangelogWindow } from "../gui/window/ChangelogWindow"
 import { ChartListWindow } from "../gui/window/ChartListWindow"
-import { CustomScriptEditorWindow } from "../gui/window/CustomScriptEditorWindow"
 import { EQWindow } from "../gui/window/EQWindow"
 import { ExportNotedataWindow } from "../gui/window/ExportNotedataWindow"
 import { GameplayKeybindWindow } from "../gui/window/GameplayKeybindWindow"
@@ -1711,14 +1710,14 @@ export const KEYBIND_DATA: { [key: string]: Keybind } = {
       window.nw.Window.open(window.location.href)
     },
   },
-  editCustomScripts: {
-    label: "Edit custom scripts...",
-    combos: [],
-    disabled: false,
-    callback: app => {
-      app.windowManager.openWindow(new CustomScriptEditorWindow(app))
-    },
-  },
+  // editCustomScripts: {
+  //   label: "Edit custom scripts...",
+  //   combos: [],
+  //   disabled: false,
+  //   callback: app => {
+  //     app.windowManager.openWindow(new CustomScriptEditorWindow(app))
+  //   },
+  // },
 }
 
 // Dynamically add keybinds
